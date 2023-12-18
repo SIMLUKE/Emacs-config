0fb3db255088098df0ab0d10ba6eae4503132d05
SIMLUK <luc.simon@epitech.eu>
2023-12-18 22:48:27 +0100

    [ADD] added more shortcuts

--- custom/keyboard-shortcuts.el
+++ custom/keyboard-shortcuts.el
@@ -2,6 +2,7 @@
 (global-set-key "\C-q" 'tab-bar-close-tab)
 (global-set-key "\C-cr" 'replace-string)
 (global-set-key "\C-cp" 'company-mode)
+(global-set-key "\C-cv" 'vterm-copy-mode)
 (global-set-key "\C-ct" 'my-open-vterm)
 (global-set-key "\C-cs" 'treemacs-select-directory)
 (global-set-key "\C-xs" 'switch-to-buffer-other-frame)
