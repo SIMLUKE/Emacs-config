79021282d530fa35c0864955e5571269c6b05e87
Luc Simon <luc.simon@epitech.eu>
2024-01-29 15:23:28 +0100

    [FIX] msic

--- restart_emacs.sh
+++ restart_emacs.sh
@@ -3,11 +3,11 @@
 pgrep_output=$(pgrep emacs)
 pgrep_arr=($pgrep_output)
 if [[ "${#pgrep_arr[@]}" == "1" ]] || [[ "${#pgrep_arr[@]}" == "0" ]]; then
-    echo "emacs is not running. Starting emacs."
+    echo "emacs is not running, starting ..."
     emacs --with-profile tiny --daemon=tiny
     emacs --daemon
 else
-    echo "emacs is running. restarting emacs."
+    echo "emacs is running, restarting ..."
     killall emacs
     emacs --with-profile tiny --daemon=tiny
     emacs --daemon
