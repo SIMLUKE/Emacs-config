193200bfc4b625c80c0e129f96a744c73fc27eb8
SIMLUKE <luc.simon@epitech.eu>
2024-11-06 15:31:30 +0100

    fix: fixed restore script

--- restore_old_emacs.sh
+++ restore_old_emacs.sh
@@ -1,5 +1,11 @@
+emacs_files=(".emacs.d" ".emacs.default" ".emacs-profiles.el" ".emacs.tiny")
+new_old_dir="~/old_emacs/config1/"
 echo "Restoring old emacs ..."
-mkdir ~/old_emacs/config1
-mv ~/.emacs* ~/old_emacs/config1/
-mv ~/old_emacs/* ~/
+mkdir $new_old_dir
+
+for file in "${emacs_files[@]}"; do
+    mv ~/$file ~/old_emacs/config1/
+    mv ~/old_emacs/$file ~/
+done
+
 echo "Done."
