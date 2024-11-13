fc23ebc7cae6b1954961da235fe175454bc98801
SIMLUKE <luc.simon@epitech.eu>
2024-11-13 21:42:57 +0100

    fix: fixed restore emacs script

--- restore_old_emacs.sh
+++ restore_old_emacs.sh
@@ -1,11 +1,25 @@
-emacs_files=(".emacs.d" ".emacs.default" ".emacs-profiles.el" ".emacs.tiny")
-new_old_dir="~/old_emacs/config1/"
+#!/bin/bash
+
+if [ $# -ne 0 ]; then
+    if [ ! -d $HOME/old_emacs/config$1 ]; then
+        echo "Error : $HOME/old_emacs/config$1 does not exist"
+        exit 1
+    fi
+    echo "Restoring config$1"
+    mv $HOME/old_emacs/config$1/.emacs* $HOME
+    rmdir "$HOME/old_emacs/config$1"
+    exit 0
+fi
+latestconfig=$(ls $HOME/old_emacs | grep config | sed -n 's/^config//p' | sort -n | tail -1)
+newconfig=$((latestconfig + 1))
+
+new_old_dir="$HOME/old_emacs/config$newconfig"
+
 echo "Restoring old emacs ..."
-mkdir $new_old_dir
+if [ ! -d $new_old_dir ]; then
+    mkdir $new_old_dir
+fi
 
-for file in "${emacs_files[@]}"; do
-    mv ~/$file ~/old_emacs/config1/
-    mv ~/old_emacs/$file ~/
-done
-
+mv $HOME/.emacs* $new_old_dir
+mv $HOME/old_emacs/.emacs* $HOME
 echo "Done."
